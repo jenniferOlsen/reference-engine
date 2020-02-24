77f0b4b1250262837e6b99c1216ef578ed782a3c
Jenny Olsen <jenny.a.olsen@gmail.com>
2020-02-24 14:55:26 -0800

    randomize images

--- src/ImageDisplay.js
+++ src/ImageDisplay.js
@@ -11,21 +11,62 @@ export default class ImageDisplay extends Component {
     this.state = {
       photoIndex: 0,
       isOpen: false,
+      images: files,
     };
   }
 
+  /**
+   * Randomly shuffle an array
+   * https://stackoverflow.com/a/2450976/1293256
+   * @param  {Array} array The array to shuffle
+   * @return {String}      The first item in the shuffled array
+   */
+
+    shuffle = (array) => {
+      let currentIndex = array.length;
+      let temporaryValue, randomIndex;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+	return array;
+
+};
+
+  shuffleFiles = () => {
+    let images = []
+    let length = files.length - 1;
+    let index = [...Array(length).keys()]
+    let keys = Object.keys(files);
+
+    this.shuffle(index)
+    keys.sort(function(a,b) {return Math.random() - 0.5;});
+    keys.forEach(function(k) {images.push(files[k]);});
+    this.setState({ images: images }) 
+  }
+
   displayImages = () => {
+    this.shuffleFiles()
     this.setState({ isOpen: true })
-}
+  }
 
   componentDidMount = () => {
     getFiles()
   }
 
   render() {
-    const { photoIndex, isOpen } = this.state;
+    const { photoIndex, isOpen, images } = this.state;
     const base64 = 'data:image/jpeg;base64,'
-
+    console.log(images)
     return (
       <div>
         <Button
@@ -37,20 +78,20 @@ export default class ImageDisplay extends Component {
           Start
         </Button>
     
-        {files.length > 0 && isOpen && (
+        {images.length > 0 && isOpen && (
           <Lightbox
-            mainSrc={base64+files[photoIndex].thumbnail}
-            nextSrc={base64+files[(photoIndex + 1) % files.length].thumbnail}
-            prevSrc={base64+files[(photoIndex + files.length - 1) % files.length].thumbnail}
+            mainSrc={base64+images[photoIndex].thumbnail}
+            nextSrc={base64+images[(photoIndex + 1) % images.length].thumbnail}
+            prevSrc={base64+images[(photoIndex + images.length - 1) % images.length].thumbnail}
             onCloseRequest={() => this.setState({ isOpen: false })}
             onMovePrevRequest={() =>
               this.setState({
-                photoIndex: (photoIndex + files.length - 1) % files.length,
+                photoIndex: (photoIndex + images.length - 1) % images.length,
               })
             }
             onMoveNextRequest={() =>
               this.setState({
-                photoIndex: (photoIndex + 1) % files.length,
+                photoIndex: (photoIndex + 1) % images.length,
               })
             }
           />
