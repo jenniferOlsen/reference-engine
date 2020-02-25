e4448af2c94f3ff609bcde2a81a2b8af90c8621f
Jenny Olsen <jenny.a.olsen@gmail.com>
2020-02-25 10:30:40 -0800

    get files recursively

--- src/files.js
+++ src/files.js
@@ -15,15 +15,25 @@ function filterByValue(array, string) {
 }
 
 export let files = [];
-export const getFiles = () => { 
-    dbx.filesListFolder({  
-    path: '',
-    recursive: true,
-    include_non_downloadable_files: false,  
-  }).then(response => {
-       dbx.filesGetThumbnailBatch(
+export const getFiles = async () => { 
+   const filesData = await dbx.filesListFolder({  
+      path: '',
+      recursive: true,
+      include_non_downloadable_files: false,  
+      limit: 25 // thumbnail batch limit imposed by api
+  })
+
+  processFiles(filesData.entries)
+
+  if (filesData.has_more) {
+    getMoreFiles(filesData.cursor, more => processFiles(more.entries))
+  }
+}
+
+const processFiles = async(response) => {
+   dbx.filesGetThumbnailBatch(
          {    
-          entries: response.entries.map(function(entry){
+          entries: response.map(function(entry){
             return {
               path: entry.id,
               format : 'jpeg',
@@ -32,11 +42,20 @@ export const getFiles = () => {
         })
       }).then(response => {
         let data = response.entries;
-        return files = filterByValue(data, 'success')
+        let filtered = filterByValue(data, 'success')
+        return files = files.concat(filtered)
       })
-  })
 }
 
+const getMoreFiles = async (cursor, callback) => {  
+  const response = await dbx.filesListFolderContinue({ cursor }) 
+  if (callback) callback(response)
+  if (response.has_more) {  
+      // if there are more files, call getMoreFiles recursively,  
+      // providing the same callback.  
+      await getMoreFiles(response.cursor, callback)  
+    }  
+}
   
 
 
