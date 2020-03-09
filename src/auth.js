6ebe07506df61902d5ca884b107306894497625f
Jenny Olsen <jenny.a.olsen@gmail.com>
2020-03-09 13:37:56 -0700

    authlink as variable

--- src/auth.js
+++ src/auth.js
@@ -1,14 +1,14 @@
 import { Dropbox } from 'dropbox';
 import { parseQueryString } from './utlis'
 
-export const CLIENT_ID = process.env.REACT_APP_DROPBOX_TOKEN;;
+export const CLIENT_ID = process.env.REACT_APP_DROPBOX_TOKEN;
 
 const dbx = new Dropbox({  
   clientId: CLIENT_ID,
   fetch  
 });
 
-export let authlink = dbx.getAuthenticationUrl('http://localhost:3000/auth');
+export let authlink = dbx.getAuthenticationUrl(process.env.REACT_APP_DROPBOX_AUTHLINK);
 export let startButtonText = 'Start'
 
 // Parses the url and gets the access token if it is in the urls hash
